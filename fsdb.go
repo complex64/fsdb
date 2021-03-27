540e66cb2ec3fb30f53b47403e4bec0e0b653362
Joe <joe@complex64.com>
2021-03-27 16:11:49 +0100

    add ReadAll, introduce Key()

--- fsdb.go
+++ fsdb.go
@@ -1,14 +1,24 @@
 package fsdb
 
 import (
+	"context"
 	"fmt"
+	"io/fs"
 	"io/ioutil"
 	"os"
 	"path"
+	"path/filepath"
+	"reflect"
+	"runtime"
+	"strings"
 	"sync"
 	"syscall"
 
 	"gopkg.in/yaml.v3"
+)
+
+const (
+	Extension = ".yaml"
 )
 
 func New(dataDir string) *DB {
@@ -23,10 +33,9 @@ type DB struct {
 	lock    *sync.RWMutex
 }
 
-func (db *DB) Read(out interface{}, key ...string) (bool, error) {
-	loc := db.filepath(key...)
+func (db *DB) Read(key Key, out interface{}) (bool, error) {
 	db.lock.RLock()
-	bs, err := ioutil.ReadFile(loc)
+	bs, err := ioutil.ReadFile(key.Document())
 	db.lock.RUnlock()
 
 	if pErr, ok := err.(*os.PathError); ok && pErr.Err == syscall.ENOENT {
@@ -42,10 +51,7 @@ func (db *DB) Read(out interface{}, key ...string) (bool, error) {
 	return true, nil
 }
 
-func (db *DB) Write(in interface{}, key ...string) error {
-	loc := db.filepath(key...)
-	dir := path.Dir(loc)
-
+func (db *DB) Write(key Key, in interface{}) error {
 	bs, err := yaml.Marshal(in)
 	if err != nil {
 		return err
@@ -53,21 +59,242 @@ func (db *DB) Write(in interface{}, key ...string) error {
 
 	db.lock.Lock()
 	defer db.lock.Unlock()
-	if err := os.MkdirAll(dir, 0755); err != nil {
+	if err := os.MkdirAll(key.Prefix(), 0755); err != nil {
 		return err
 	}
-	return ioutil.WriteFile(loc, bs, 0644)
+	return ioutil.WriteFile(key.Document(), bs, 0644)
 }
 
-func (db *DB) Delete(key ...string) error {
-	loc := db.filepath(key...)
+func (db *DB) Delete(key Key) error {
 	db.lock.Lock()
 	defer db.lock.Unlock()
-	return os.RemoveAll(loc)
+	return os.RemoveAll(key.Document())
 }
 
-func (db *DB) filepath(key ...string) string {
-	key[len(key)-1] = fmt.Sprintf("%s.yaml", key[len(key)-1])
-	elem := append([]string{db.dataDir}, key...)
-	return path.Join(elem...)
+func (db *DB) ReadAll(key Key, keys *[]Key, values interface{}) error {
+	db.lock.RLock()
+	entries, err := db.readAll(key)
+	db.lock.RUnlock()
+
+	if err != nil {
+		return err
+	}
+
+	sliceType := reflect.ValueOf(values).Elem().Type()
+	slice := reflect.MakeSlice(sliceType, 0, len(entries))
+	ks := make([]Key, 0, len(entries))
+
+	fmt.Println(slice)
+
+	var (
+		routines = runtime.NumCPU()
+		inputs   = make(chan encoded, routines)
+		outputs  = make(chan decoded, routines)
+	)
+
+	go func() {
+		defer close(inputs)
+		for _, e := range entries {
+			inputs <- e
+		}
+	}()
+
+	done := make(chan interface{})
+	go func() {
+		for e := range outputs {
+			ks = append(ks, e.key)
+			slice = reflect.Append(slice, reflect.ValueOf(e.value))
+		}
+		done <- struct{}{}
+	}()
+
+	valueType := sliceType.Elem()
+	ctx, errs := db.decodeAsync(routines, inputs, outputs, valueType)
+	<-ctx.Done()
+
+	if err, ok := <-errs; !ok && err != nil {
+		return err
+	}
+
+	<-done
+	*keys = ks
+	reflect.ValueOf(values).Elem().Set(slice)
+	return nil
+}
+
+func (db *DB) decodeAsync(
+	routines int,
+	inputs <-chan encoded,
+	outputs chan<- decoded,
+	valueType reflect.Type,
+) (
+	context.Context,
+	<-chan error,
+) {
+	errs := make(chan error, routines)
+	ctx, cancel := context.WithCancel(context.Background())
+	wg := &sync.WaitGroup{}
+	wg.Add(routines)
+
+	go func() {
+		wg.Wait()
+		close(errs)
+		close(outputs)
+		cancel()
+	}()
+
+	for i := 0; i < routines; i++ {
+		go func() {
+			defer wg.Done()
+			for entry := range inputs {
+				zero := reflect.Zero(valueType)
+				ptr := reflect.New(valueType)
+				ptr.Elem().Set(zero)
+
+				err := yaml.Unmarshal(entry.bytes, ptr.Interface())
+				if err != nil {
+					errs <- err
+					return
+				}
+
+				outputs <- decoded{
+					key:   db.parseKey(entry.path),
+					value: ptr.Elem().Interface(),
+				}
+			}
+		}()
+	}
+
+	return ctx, errs
+}
+
+func (db *DB) readAll(key Key) ([]encoded, error) {
+	var (
+		bufSize  = 256
+		routines = runtime.NumCPU()
+
+		inputs  = make(chan string, bufSize)
+		outputs = make(chan encoded, routines)
+		entries = make([]encoded, 0, bufSize)
+	)
+
+	done := make(chan interface{})
+	go func() {
+		for entry := range outputs {
+			entries = append(entries, entry)
+		}
+		done <- struct{}{}
+	}()
+
+	walkCtx, walkErrs := db.walkAsync(key, inputs)
+	readCtx, readErrs := db.readAsync(routines, inputs, outputs)
+	<-walkCtx.Done()
+	<-readCtx.Done()
+
+	if err, ok := <-readErrs; !ok && err != nil {
+		return nil, err
+	}
+
+	if err, ok := <-walkErrs; !ok && err != nil {
+		return nil, err
+	}
+
+	<-done
+	return entries, nil
+}
+
+func (db *DB) walkAsync(key Key, paths chan<- string) (context.Context, <-chan error) {
+	ctx, cancel := context.WithCancel(context.Background())
+	errs := make(chan error, 1)
+	go func() {
+		defer cancel()
+		defer close(paths)
+		defer close(errs)
+		err := filepath.Walk(key.Prefix(), func(path string, info fs.FileInfo, e error) error {
+			if e != nil {
+				return e
+			}
+			if info.IsDir() || !strings.HasSuffix(path, Extension) {
+				return nil
+			}
+			paths <- path
+			return nil
+		})
+		if err != nil {
+			errs <- err
+		}
+		return
+	}()
+	return ctx, errs
+}
+
+func (db *DB) readAsync(routines int, paths <-chan string, entries chan<- encoded) (context.Context, <-chan error) {
+	ctx, cancel := context.WithCancel(context.Background())
+	errs := make(chan error, routines)
+	wg := &sync.WaitGroup{}
+	wg.Add(routines)
+
+	go func() {
+		wg.Wait()
+		close(errs)
+		close(entries)
+		cancel()
+	}()
+
+	for i := 0; i < routines; i++ {
+		go func() {
+			defer wg.Done()
+			for p := range paths {
+				bs, err := ioutil.ReadFile(p)
+				if err != nil {
+					errs <- err
+					return
+				}
+				entries <- encoded{
+					path:  p,
+					bytes: bs,
+				}
+			}
+		}()
+	}
+
+	return ctx, errs
+}
+
+func (db *DB) parseKey(s string) Key {
+	return Key{
+		prefix: strings.TrimSuffix(s, Extension),
+		doc:    s,
+	}
+}
+
+func (db *DB) Key(head string, tail ...string) Key {
+	k := make([]string, 0, 2+len(tail))
+	k = append(k, db.dataDir)
+	k = append(k, head)
+	k = append(k, tail...)
+	prefix := path.Join(k...)
+	doc := prefix + Extension
+	return Key{
+		prefix: prefix,
+		doc:    doc,
+	}
+}
+
+type Key struct {
+	prefix string
+	doc    string
+}
+
+func (k *Key) Prefix() string   { return k.prefix }
+func (k *Key) Document() string { return k.doc }
+
+type encoded struct {
+	path  string
+	bytes []byte
+}
+
+type decoded struct {
+	key   Key
+	value interface{}
 }
--- fsdb_test.go
+++ fsdb_test.go
@@ -3,6 +3,7 @@ package fsdb_test
 import (
 	"io/ioutil"
 	"os"
+	"sort"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -23,30 +24,58 @@ func TestDB(t *testing.T) {
 	// Read missing:
 
 	var doc Doc
-	ok, err := db.Read(&doc, "foo", "bar")
+	ok, err := db.Read(db.Key("foo", "bar"), &doc)
 	assert.NoError(t, err)
 	assert.False(t, ok)
 
 	// Write new:
 
 	written := Doc{Field: "value"}
-	assert.NoError(t, db.Write(written, "bar", "baz"))
+	assert.NoError(t, db.Write(db.Key("bar", "baz"), written))
+
+	written2 := Doc{Field: "value2"}
+	assert.NoError(t, db.Write(db.Key("bar", "baz2"), written2))
 
 	// Read existing:
 
 	var read Doc
-	ok, err = db.Read(&read, "bar", "baz")
+	ok, err = db.Read(db.Key("bar", "baz"), &read)
 	assert.NoError(t, err)
 	assert.True(t, ok)
 	assert.Equal(t, written, read)
 
+	// Read all:
+
+	var keys []fsdb.Key
+	var values []Doc
+	err = db.ReadAll(db.Key("bar"), &keys, &values)
+
+	assert.NoError(t, err)
+	assert.Len(t, keys, 2)
+	assert.Len(t, values, 2)
+
+	sort.Slice(keys, func(i, j int) bool { return keys[i].Document() < keys[j].Document() })
+	sort.Slice(values, func(i, j int) bool { return values[i].Field < values[j].Field })
+
+	assert.Equal(t, db.Key("bar", "baz"), keys[0])
+	assert.Equal(t, db.Key("bar", "baz2"), keys[1])
+	assert.Equal(t, written, values[0])
+	assert.Equal(t, written2, values[1])
+
 	// Remove missing and existing:
 
-	assert.NoError(t, db.Delete("bogus"))
-	assert.NoError(t, db.Delete("bar", "baz"))
+	assert.NoError(t, db.Delete(db.Key("bogus")))
+	assert.NoError(t, db.Delete(db.Key("bar", "baz")))
 
 	var gone Doc
-	ok, err = db.Read(&gone, "bar", "baz")
+	ok, err = db.Read(db.Key("bar", "baz"), &gone)
 	assert.False(t, ok)
 	assert.NoError(t, err)
+}
+
+func TestKey(t *testing.T) {
+	db := fsdb.New("/tmp")
+	key := db.Key("foo", "bar")
+	assert.Equal(t, "/tmp/foo/bar", key.Prefix())
+	assert.Equal(t, "/tmp/foo/bar"+fsdb.Extension, key.Document())
 }
